cb24c497122ce8e76eaa689421d3db5930aaaa6c
Huzaifa0888 <115368837+Huzaifa0888@users.noreply.github.com>
2023-04-11 00:52:06 +0500

    done

--- components/ourWork.jsx
+++ components/ourWork.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 function OurWork() {
   return (
     <>
-      <section className="py-10 sm:py-16 lg:py-24 bg-white">
+      <section className="py-10 sm:py-16 lg:py-24 bg-white hidden lg:block">
         <div className="px-4 mx-auto max-w-[1150px] sm:px-6 lg:px-8">
           <div>
             <h1 className="text-4xl text-center font-normal text-blue-400 sm:text-6xl pb-5 lg:text-[30px]">
@@ -14,7 +14,7 @@ function OurWork() {
             </h1>
           </div>
           <section className=" ">
-            <div className="px-4 mx-auto max-w-7xl sm:px-6 lg:px-8">
+            <div className="px-4  ">
               <div className="grid max-w-xl  grid-cols-1 mx-auto mt-8 text-center lg:max-w-full sm:mt-12 lg:mt-20 lg:grid-cols-3 gap-x-0 xl:gap-x-12 gap-y-20">
                 <div className="bg-sec h-[340px] w-full ">
                   <div className="overflow-hidden h-[280px] w-[280px] mt-8 ml-8 items-center rounded-full shadow shadow-gray-400">
