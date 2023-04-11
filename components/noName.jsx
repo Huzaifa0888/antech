9b592a849711e4afb5d8def5fed1eadbfe0c377e
Huzaifa0888 <115368837+Huzaifa0888@users.noreply.github.com>
2023-04-12 00:57:21 +0500

    done

--- components/noName.jsx
+++ components/noName.jsx
@@ -5,15 +5,15 @@ function NoName() {
     <>
       <section className="py-10 sm:py-16  lg:pb-[110px] lg:pt-0 lg:mt-0">
         <div className="px-4 mx-auto max-w-[1150px] sm:px-6 lg:px-0">
-          <div className="grid grid-cols-1 lg:h-[500px]  mt-12 lg:mt-24 gap-y-12 md:grid-cols-3 ">
+          <div className="grid grid-cols-1 lg:h-[450px]  mt-12 lg:mt-24 gap-y-12 md:grid-cols-3 ">
             <div className="md:px-4 lg:px-2 h-full w-full">
-              <img className="- h-full w-[100%]" src="/portfolio3.jpg" alt="" />
+              <img className="- h-full w-[90%]" src="/portfolio3.jpg" alt="" />
             </div>
             <div className="md:px-4 lg:px-2  w-full">
-              <img className="- h-full w-[100%]" src="/portfolio3.jpg" alt="" />
+              <img className="- h-full w-[90%]" src="/portfolio3.jpg" alt="" />
             </div>
             <div className="md:px-4 lg:px-2 h-full w-full">
-              <img className="- h-full w-[100%]" src="/portfolio3.jpg" alt="" />
+              <img className="- h-full w-[90%]" src="/portfolio3.jpg" alt="" />
             </div>
           </div>
         </div>
