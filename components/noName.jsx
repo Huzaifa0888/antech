396e8e88a0eb59e7f705b053d5b89dad4459bd87
Huzaifa0888 <115368837+Huzaifa0888@users.noreply.github.com>
2023-04-12 00:27:15 +0500

    done

--- components/noName.jsx
+++ components/noName.jsx
@@ -7,13 +7,13 @@ function NoName() {
         <div className="px-4 mx-auto max-w-[1150px] sm:px-6 lg:px-0">
           <div className="grid grid-cols-1 lg:h-[500px]  mt-12 lg:mt-24 gap-y-12 md:grid-cols-3 ">
             <div className="md:px-4 lg:px-2 h-full w-full">
-              <img className="- h-full w-[100%]" src="/portfolio1.jpg" alt="" />
+              <img className="- h-full w-[100%]" src="/portfolio3.jpg" alt="" />
             </div>
             <div className="md:px-4 lg:px-2  w-full">
-              <img className="- h-full w-[100%]" src="/portfolio1.jpg" alt="" />
+              <img className="- h-full w-[100%]" src="/portfolio3.jpg" alt="" />
             </div>
             <div className="md:px-4 lg:px-2 h-full w-full">
-              <img className="- h-full w-[100%]" src="/portfolio1.jpg" alt="" />
+              <img className="- h-full w-[100%]" src="/portfolio3.jpg" alt="" />
             </div>
           </div>
         </div>
